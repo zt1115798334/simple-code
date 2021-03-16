800c928aa3193568502b287e034666334941f0d5
zhangtong9498@qq.com <devil1994>
2021-03-16 20:37:25 +0800

    msfg

--- src/main/java/com/zt/fun/FormatWordXml.java
+++ src/main/java/com/zt/fun/FormatWordXml.java
@@ -1,6 +1,7 @@
 package com.zt.fun;
 
 import com.google.common.base.Objects;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.ResourceLoader;
 
@@ -10,56 +11,47 @@ import java.nio.file.Paths;
 import java.util.Stack;
 
 public class FormatWordXml {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        try {
+            ResourceLoader resourceLoader = new DefaultResourceLoader();
+            String document = resourceLoader.getResource("classpath:document.xml").getFile().getPath();
+            String documentFormat = resourceLoader.getResource("classpath:documentFormat.xml").getFile().getPath();
+            System.out.println("document = " + document);
+            System.out.println("documentFormat = " + documentFormat);
+            String documentContent = String.join("\n", Files.readAllLines(Paths.get(document)));
+            StringBuilder result = new StringBuilder();
+            Stack<String> stack_m = new Stack<>();
+            Stack<String> stack_n = new Stack<>();
+            StringBuilder p = new StringBuilder();
+            for (int i = 0; i < documentContent.length(); i++) {
+                String charText = String.valueOf(documentContent.charAt(i));
+                if (stack_m.isEmpty()) {
+                    result.append(charText);
+                    if (Objects.equal(charText, "$")) {
+                        stack_m.push("$");
+                    }
+                } else {
+                    if (Objects.equal(charText, "<")) {
+                        stack_n.push("<");
+                    }
+                    if (Objects.equal(charText, ">")) {
+                        stack_n.pop();
 
-        ResourceLoader resourceLoader = new DefaultResourceLoader();
-        String document = resourceLoader.getResource("classpath:document.xml").getFile().getPath();
-        System.out.println("document = " + document);
-        String documentContent = String.join("\n", Files.readAllLines(Paths.get(document)));
-//        System.out.println(collect);
-        StringBuilder result = new StringBuilder();
-        Stack<String> stack = new Stack<>();
-        int top = 0;
-        int bottom = 0;
-        StringBuilder p = new StringBuilder();
-        for (int i = 0; i < documentContent.length(); i++) {
-            String charText = String.valueOf(documentContent.charAt(i));
-            System.out.println("charText: " + charText + " top: " + top + " buttom:" + bottom);
-            if (Objects.equal(charText, "<")) {
-                stack.push("<");
-                bottom = i - 1;
-                if ((top - 1) >= 0) {
-                    result.append(documentContent.substring(top, bottom));
+                    }
+                    if ((!Objects.equal(charText, "<") && !Objects.equal(charText, ">"))
+                            && stack_n.isEmpty()&& StringUtils.isNotBlank(charText)) {
+                        result.append(charText);
+                    }
+                    if (Objects.equal(charText, "}")) {
+                        stack_m.pop();
+                    }
                 }
             }
-            if (Objects.equal(charText, ">")) {
-                String pop = stack.pop();
-            }
-            //遍历到”>“元素
-            if (stack.isEmpty()) {
-                p = new StringBuilder();
-            } else {
-                top = i;
-                p.append(charText);
-            }
-
-            if (Objects.equal(charText, "$")) {
-                System.out.println("$ = " + i);
-                bottom = i;
-                result.append(documentContent.substring(top, bottom));
-                top = i + 1;
-            }
-            if (Objects.equal(charText, "{")) {
-                System.out.println("{ = " + i);
-                top = i;
-                bottom = i + 1;
-                result.append(documentContent.substring(top, bottom));
-            }
-            if (Objects.equal(charText, "}")) {
-                System.out.println("} = " + i);
-
-            }
+//            System.out.println(result.toString());
+            Files.write(Paths.get(documentFormat), result.toString().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        System.out.println(result.toString());
+
     }
 }
